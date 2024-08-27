1f0f79827593b2cac432505c109c49631d072c1f
eugene0318 <nttott@gmail.com>
2024-08-28 06:14:37 +0900

    ~ 검증1 끝.

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -11,6 +11,9 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -25,6 +28,12 @@ import java.util.Map;
 public class ValidationItemControllerV2 {
 
 	private final ItemRepository itemRepository;
+	private final ItemValidator itemValidator;
+	
+	@InitBinder
+	public void init(WebDataBinder dataBinder) {
+		dataBinder.addValidators(itemValidator);
+	}
 
 	@GetMapping
 	public String items(Model model) {
@@ -151,14 +160,21 @@ public class ValidationItemControllerV2 {
 		return "redirect:/validation/v2/items/{itemId}";
 	}
 
-	@PostMapping("/add")
+//	@PostMapping("/add")
 	public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult,
 			RedirectAttributes redirectAttributes) {
+
+		if (bindingResult.hasErrors()) {
+			return "validation/v2/addForm";
+		}
 		log.info("objectName={}", bindingResult.getObjectName());
 		log.info("target={}", bindingResult.getTarget());
-		if (!StringUtils.hasText(item.getItemName())) {
-			bindingResult.rejectValue("itemName", "required");
-		}
+//		if (!StringUtils.hasText(item.getItemName())) {
+//			bindingResult.rejectValue("itemName", "required");
+//		}
+
+		ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult, "itemName", "required");
+
 		if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
 			bindingResult.rejectValue("price", "range", new Object[] { 1000, 1000000 }, null);
 		}
@@ -176,6 +192,40 @@ public class ValidationItemControllerV2 {
 			log.info("errors={}", bindingResult);
 			return "validation/v2/addForm";
 		}
+		// 성공 로직
+		Item savedItem = itemRepository.save(item);
+		redirectAttributes.addAttribute("itemId", savedItem.getId());
+		redirectAttributes.addAttribute("status", true);
+		return "redirect:/validation/v2/items/{itemId}";
+	}
+
+//	@PostMapping("/add")
+	public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult,
+			RedirectAttributes redirectAttributes) {
+
+		itemValidator.validate(item, bindingResult);
+
+		if (bindingResult.hasErrors()) {
+			log.info("errors={}", bindingResult);
+			return "validation/v2/addForm";
+		}
+		
+		// 성공 로직
+		Item savedItem = itemRepository.save(item);
+		redirectAttributes.addAttribute("itemId", savedItem.getId());
+		redirectAttributes.addAttribute("status", true);
+		return "redirect:/validation/v2/items/{itemId}";
+	}
+	
+	@PostMapping("/add")
+	public String addItemV6(@Validated @ModelAttribute Item item, BindingResult bindingResult,
+			RedirectAttributes redirectAttributes) {
+
+		if (bindingResult.hasErrors()) {
+			log.info("errors={}", bindingResult);
+			return "validation/v2/addForm";
+		}
+		
 		// 성공 로직
 		Item savedItem = itemRepository.save(item);
 		redirectAttributes.addAttribute("itemId", savedItem.getId());
